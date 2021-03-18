888f6d8b98b3298efa808cb0c7dc9f9f2c197959
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-03-18 20:44:53 -0300

    att

--- trab1/marteloThor.dart
+++ trab1/marteloThor.dart
@@ -1,58 +1,47 @@
 // 3 - marteloThor(thor, martelo): receberá duas listas com dois elementos, representando a posição de thor e a posição do seu martelo em um plano bidimensional. Ao final, deverá imprimir uma lista de coordenadas que o thor deverá visitar até chegar no seu martelo. O thor só pode se mover para norte, sul, leste ou oeste.
 
-marteloThor(List thor, List martelo) {
-  // if((thor[0] - martelo[0]) < (thor[1] - martelo[1])){
-  //   //aproximar o eixo X
-  // }else{
+marteloThor(var thor, var martelo) {
+  // var distanciaEixoX = (martelo[0] - thor[0]);
+  // var distanciaEixoY = (martelo[1] - thor[1]);
+  int thorX = thor[0];
+  int thorY = thor[1];
+  int marteloX = martelo[0];
+  int marteloY = martelo[1];
 
-  // }
+  List<int> pathX = [];
+  List<int> pathY = [];
 
-  for (thor[0]; thor[0] < martelo[0]; thor[0]++) {
-    for (thor[1]; thor[1] < martelo[1]; thor[1]++) {
-      print("[${thor[0]}, ${thor[1]}");
+  if (thorX >= marteloX) {
+    for (var i = thorX; i >= marteloX; i--) {
+      pathX.add(i);
+    }
+  } else {
+    for (var i = thorX; i <= marteloX; i++) {
+      pathX.add(i);
+    }
+  }
+
+  if (thorY >= marteloY) {
+    for (var i = thorY; i >= marteloY; i--) {
+      pathY.add(i);
+    }
+  } else {
+    for (var i = thorY; i <= marteloY; i++) {
+      pathY.add(i);
+    }
+  }
+
+  int aux = 0;
+  for (var j = 0; j < pathY.length; j++) {
+    if (j < pathX.length) {
+      j == 1 ? aux = j : aux = j + 1;
+      print("[${pathX[aux]}, ${pathY[j]}]");
+    } else {
+      print("[${pathX[aux]}, ${pathY[j]}]");
     }
   }
 }
 
 void main() {
   marteloThor([5, 2], [4, 7]);
-  print("oi");
-  // [4, 2]
-  // [4, 3]
-  // [4, 4]
-  // [4, 5]
-  // [4, 6]
-  // [4, 7]
-
-  marteloThor([9, 7], [11, 3]);
-  print("oi");
-  // [10, 7]
-  // [10, 6]
-  // [11, 6]
-  // [11, 5]
-  // [11, 4]
-  // [11, 3]
-  //
-  marteloThor([5, 7], [-5, -3]);
-  print("oi");
-  // [4, 7]
-  // [4, 6]
-  // [3, 6]
-  // [3, 5]
-  // [2, 5]
-  // [2, 4]
-  // [1, 4]
-  // [1, 3]
-  // [0, 3]
-  // [0, 2]
-  // [-1, 2]
-  // [-1, 1]
-  // [-2, 1]
-  // [-2, 0]
-  // [-3, 0]
-  // [-3, -1]
-  // [-4, -1]
-  // [-4, -2]
-  // [-5, -2]
-  // [-5, -3]
 }
