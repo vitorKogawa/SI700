4b630f4f42f0d29d78091b9bea8ed689ed174fd2
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-05-16 20:16:54 -0300

    implementando model de usuário

---
+++ entrega03/exercicio03/lib/models/User.dart
@@ -0,0 +1,8 @@
+class User {
+  final String email;
+  final String firstName;
+  final String lastName;
+  final String password;
+
+  const User({this.email, this.firstName, this.lastName, this.password});
+}
