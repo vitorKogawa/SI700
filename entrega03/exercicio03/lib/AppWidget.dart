02caed83627aa78b7d9ab3fcdcf99de9c6d73ca5
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 14:16:53 -0300

    add app routes

--- entrega03/exercicio03/lib/AppWidget.dart
+++ entrega03/exercicio03/lib/AppWidget.dart
@@ -1,4 +1,5 @@
 import 'package:exercicio03/controller/AppController.dart';
+import 'package:exercicio03/routes/AppRoutes.dart';
 import 'package:exercicio03/view/DetalhesLivro/DetalhesLivro.dart';
 import 'package:exercicio03/view/HomePage/HomePage.dart';
 import 'package:exercicio03/view/Sobre/Sobre.dart';
@@ -21,11 +22,11 @@ class AppWidget extends StatelessWidget {
                     : Brightness.light),
             initialRoute: '/',
             routes: {
-              '/': (context) => LoginPage(),
-              '/home': (context) => HomePageWidget(),
-              '/about': (context) => SobreWidget(),
-              '/cadastro-usuario': (context) => CadastroUsuario(),
-              '/see-book': (context) => DetalhesLivroWidget()
+              AppRoutes.LOGIN: (context) => LoginPage(),
+              AppRoutes.HOME: (context) => HomePageWidget(),
+              AppRoutes.ABOUT: (context) => SobreWidget(),
+              AppRoutes.USER_REGISTRATION: (context) => CadastroUsuario(),
+              AppRoutes.BOOK_DETAILS: (context) => DetalhesLivroWidget()
             },
           );
         });
