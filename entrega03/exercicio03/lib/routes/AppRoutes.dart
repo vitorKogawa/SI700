4df150b7afb23b73debe3a8efa6bd534bb89429d
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 14:17:25 -0300

    definig application routes

---
+++ entrega03/exercicio03/lib/routes/AppRoutes.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class AppRoutes {
+  static final HOME = "/home";
+  static final ABOUT = "/about";
+  static final LOGIN = "/login";
+  static final USER_REGISTRATION = "/user-registration";
+  static final BOOK_DETAILS = "/book-details";
+}
