ee21c684d75a0efd824f1f3cb88767f13848b602
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 16:11:26 -0300

    definig base url

---
+++ entrega03/exercicio03/lib/services/Api.dart
@@ -0,0 +1,5 @@
+class Api {
+  static final BASE_URL = "http://localhost:3333/";
+  static final BASE_URL_SHARED_ROUTES_USER = "${BASE_URL}/shared/user";
+  static final BASE_URL_PRIVATE_ROUTES_USER = "${BASE_URL}/private/user";
+}
