7a560651e81a5237f96fdd97003c596e296c2975
Vitor Roberto Kogawa de Moraes <v245582@dac.unicamp.br>
2021-06-04 16:10:45 -0300

    adjusting login function in user controller class

--- entrega03/exercicio03/lib/controller/UserController.dart
+++ entrega03/exercicio03/lib/controller/UserController.dart
@@ -1,5 +1,23 @@
-import 'package:exercicio03/models/User.dart';
+import 'package:exercicio03/services/Api.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert' as convert;
 
 class UserController {
-  store(User user) async {}
+  login({String email, String password}) async {
+    print(email);
+    print(password);
+    // Map<String, String> creadentials = {"email": email, "password": password};
+    // Map<String, String> headers = {"Content-type": "application/json"};
+
+    // var url = Uri.parse("${Api.BASE_URL}/shared/login");
+    // var response = await http.post(url, headers: headers, body: creadentials);
+    // if (response.statusCode == 200) {
+    //   var jsonResponse =
+    //       convert.jsonDecode(response.body) as Map<String, dynamic>;
+    //   var itemCount = jsonResponse['totalItems'];
+    //   print('Number of books about http: $itemCount.');
+    // } else {
+    //   print('Request failed with status: ${response.statusCode}.');
+    // }
+  }
 }
